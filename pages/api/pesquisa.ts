1c24e57134f67ea3ed09d46d2c9402c50ab241bc
Matheus Gabriel dos Santos <168146040+SsmokDev@users.noreply.github.com>
2024-09-16 23:53:34 -0300

    Api Pesquisa

--- pages/api/pesquisa.ts
+++ pages/api/pesquisa.ts
@@ -3,6 +3,7 @@ import type {RespostaPadraoMsg} from '../../types/RespostaPadraoMsg';
 import { conectarMongoDB } from '@/middlewares/conectarMongoDB';
 import { validarTokenJWT } from '@/middlewares/validarTokenJWT';
 import { UsuarioModel } from '@/models/UsuarioModel';
+import { politicaCORS } from '@/middlewares/politicaCORS';
 
 const pesquisaEndpoint = async (req : NextApiRequest, res : NextApiResponse<RespostaPadraoMsg | any[]>) => {
     
@@ -38,4 +39,4 @@ const pesquisaEndpoint = async (req : NextApiRequest, res : NextApiResponse<Resp
     }
 }
 
-export default validarTokenJWT(conectarMongoDB(pesquisaEndpoint));
+export default politicaCORS(validarTokenJWT(conectarMongoDB(pesquisaEndpoint)));
