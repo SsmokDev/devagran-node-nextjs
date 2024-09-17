7fff2b6b6fa70dcdd5b18c623719e2f7f44e0ba2
Matheus Gabriel dos Santos <168146040+SsmokDev@users.noreply.github.com>
2024-09-16 23:54:23 -0300

    Api Feed

--- pages/api/feed.ts
+++ pages/api/feed.ts
@@ -5,48 +5,86 @@ import {RespostaPadraoMsg} from '../../types/RespostaPadraoMsg' // importando o 
 import { UsuarioModel } from '@/models/UsuarioModel'; // Importando o model do usuário
 import { PublicacaoModel } from '@/models/PublicacaoModel'; // Importando o model da publicação
 import publicacao from './publicacao';
+import { SeguidorModel } from '@/models/SeguidorModel';
+import { politicaCORS } from '@/middlewares/politicaCORS';
 
 const feedEndpoint = async (req:NextApiRequest, res: NextApiResponse<RespostaPadraoMsg | any>) => {
-    try {
-        if(req.method === 'GET'){
-            // verifica o método HTTP do request caso seja algo diferente de GET retornará que o método não é válido
-
-            if(req?.query?.id){
-                // Verifica na query se passamos algum ID para mostrarmos o Feed de um usuário específico
-
-                const usuario = await UsuarioModel.findById(req?.query?.id);// Procura na DB o usuário passado pelo ID
-                if(!usuario){
-                    // Caso não exista usuário com esse ID retorna um erro de usuário não encontrado
-                    return res.status(400).json({erro:'Usuario nao encontrado!'});
-                }
-
-                const publicacoes = await PublicacaoModel
-                .find({idUsuario : usuario._id,})
-                .sort({data : -1});
-                const result = [];
-
-                for (const publicacao of publicacoes){
-                    const usuarioDaPublicacao = await UsuarioModel.findById(publicacao.idUsuario);
-                    if (usuarioDaPublicacao) {
-                        const final = {
-                            ...publicacao._doc, usuario: {
-                                nome: usuarioDaPublicacao.nome,
-                                avatar: usuarioDaPublicacao.avatar,
-                            },
-                        };
-                        result.push(final);
-                    }
-                }
-                return res.status(200).json(result);
-            }
-
-            return res.status(200).json(publicacao);
+  try {
+    if(req.method === 'GET'){
+          
+      if(req?.query?.id){
+            
+        const usuario = await UsuarioModel.findById(req.query.id);
+        if (!usuario) {
+          
+          return res.status(400).json({ erro: "Usuário não encontrado" });
         }
-        return res.status(405).json({erro:'Metodo informado nao e valido!'});
-    } catch (e) {
-        console.log(e);
-    }
-    res.status(400).json({erro:'Nao foi possivel obter o feed do usuario!'});
-}
+            
+    
+        const publicacoes = await PublicacaoModel.find({
+          idUsuario: usuario._id,
+        }).sort({ data: -1 }); 
+        const result = [];
+    
+        for (const publicacao of publicacoes) {
+          
+          const usuarioDaPublicacao = await UsuarioModel.findById(
+            publicacao.idUsuario
+          ); 
+          if (usuarioDaPublicacao) {
+            
+            const final = {
+              ...publicacao._doc, 
+              usuario: { 
+                nome: usuarioDaPublicacao.nome,
+                avatar: usuarioDaPublicacao.avatar,
+              }
+            };
+            result.push(final); 
+          }
+        }
+        return res.status(200).json(result);
+      }else{
+        console.log('aqui');
+          const {userId} = req.query;
+          const usuarioLogado = await UsuarioModel.findById(userId);
+          if(!usuarioLogado){
+              return res.status(400).json({erro:'Usuario nao encontrado!'});
+          }
 
-export default validarTokenJWT(conectarMongoDB(feedEndpoint));
+        const seguidores = await SeguidorModel.find({usuarioId : usuarioLogado._id});
+
+        const seguidoresIds = seguidores.map((s) => s.usuarioSeguidoId);
+
+        const publicacoes = await PublicacaoModel.find({
+            $or : [
+                {idUsuario : usuarioLogado._id},
+                {idUsuario : seguidoresIds}
+            ]
+        })
+        .sort({data : -1});
+
+        const result = [];
+        for (const publicacao of publicacoes){
+          const usuarioDaPublicacao = await UsuarioModel.findById(publicacao.idUsuario);
+          if(usuarioDaPublicacao){
+            const final = {
+                ...publicacao._doc, usuario : {
+                    nome : usuarioDaPublicacao.nome,
+                    avatar : usuarioDaPublicacao.avatar,
+                }
+            };
+            result.push(final);
+          }
+        }
+        return res.status(200).json(result);
+      }
+    }
+      return res.status(405).json({erro:'Metodo informado nao e valido!'});
+  }catch (e){
+    console.log(e);
+  }
+  return res.status(400).json({erro:'Nao foi possivel obter o feed do usuario!'});
+};
+
+export default politicaCORS(validarTokenJWT(conectarMongoDB(feedEndpoint)));
